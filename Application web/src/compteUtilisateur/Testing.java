50b24eb7dcc513ba697f0167132e5f9202d9f150
= <=>
2021-02-14 11:45:34 +0100

    Structure du dossier commencée. Création des classes principales pour Gestion du compte client.

---
+++ Application web/src/compteUtilisateur/Testing.java
@@ -0,0 +1,9 @@
+package compteUtilisateur;
+
+public class Testing {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Application web/src/compteUtilisateur/Utilisateur.java
@@ -0,0 +1,29 @@
+package compteUtilisateur;
+
+public class Utilisateur {
+	
+	/* Caractéristiques inconnues des autres utilisateurs */
+	int id;
+	String mail;
+	
+	/* Caractéristiques connues des autres utilisateurs */
+	String statut;
+	String nom;
+	
+	/* Constructeur */
+	public Utilisateur(int pId) {
+		this.id = pId;
+		this.statut = "Utilisateur";
+		this.nom = "Non Renseigné";
+		this.mail = "Non Renseigné";
+	}
+	
+	/* Constructeur avec paramètres*/
+	public Utilisateur(int pId, String pStatut, String pNom, String pMail) {
+		this.id = pId;
+		this.statut = pStatut;
+		this.nom = pNom;
+		this.mail = pMail;
+	}
+
+}
